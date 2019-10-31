d266294abb6da3a85a559f87a04b9b370b47000c
ChairChandler <razzors1998@gmail.com>
2019-10-31 18:02:14 +0100

    Create server init connection with FTP clients by sending welcome message

--- FTP-Server.pro
+++ FTP-Server.pro
@@ -1,6 +1,6 @@
 QT -= gui
 
-CONFIG += c++11 console
+CONFIG += c++17 console
 CONFIG -= app_bundle
 
 # The following define makes your compiler emit warnings if you use
@@ -15,9 +15,15 @@ DEFINES += QT_DEPRECATED_WARNINGS
 #DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
 
 SOURCES += \
-        main.cpp
+        src/ftpcontroller.cpp \
+        src/main.cpp
 
 # Default rules for deployment.
 qnx: target.path = /tmp/$${TARGET}/bin
 else: unix:!android: target.path = /opt/$${TARGET}/bin
 !isEmpty(target.path): INSTALLS += target
+
+HEADERS += \
+    src/ReplyCodes.h \
+    src/ftpcommand.h \
+    src/ftpcontroller.h
--- src/ftpcontroller.h
+++ src/ftpcontroller.h
@@ -1,11 +1,33 @@
 #ifndef FTPCONTROLLER_H
 #define FTPCONTROLLER_H
+#include <netinet/ip.h>
+#include <QString>
+#include <QObject>
+#include <QtConcurrent/QtConcurrent>
+#include "ReplyCodes.h"
 
 
-class FTPcontroller
+class FTPcontroller: public QObject
 {
+    Q_OBJECT
+private:
+    const int QUEUE_SIZE = 10;
+    const int BUFF_SIZE = 512;
+    const ReplyCodes codes;
+    int serverSocket;
+    bool sendReplyCode(int clientSocket, QString code);
+
 public:
-    FTPcontroller();
+    FTPcontroller(QString address, int port);
+
+
+signals:
+    void start();
+    void newConnection(int clientSocket);
+
+private slots:
+    void handleConnection(int clientSocket);
+    void loop();
 };
 
 #endif // FTPCONTROLLER_H
